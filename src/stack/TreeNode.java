4c5dddf633a9b1ae49413f9d3cffe98c89c322d4
lihaolong <bistu_lhl@163.com>
2019-04-03 07:37:44 +0800

    add

---
+++ src/stack/TreeNode.java
@@ -0,0 +1,16 @@
+package stack;
+
+/**
+ * @program: leetcode
+ * @description:
+ * @author: lihaolong
+ * @create: 2019-01-06 21:34
+ **/
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) {
+        val = x;
+    }
+}
